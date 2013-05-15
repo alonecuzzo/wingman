37ad189c80fedf6b269efc11a097a72c903049b9
Justin Martin <lupusdei108@gmail.com>
2013-05-15 18:02:18 -0400

    Initial Commit

---
+++ wingman/WMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WMAppDelegate.h
+//  wingman
+//
+//  Created by Justin Martin on 5/15/13.
+//  Copyright (c) 2013 Two Wing Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ wingman/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  wingman
+//
+//  Created by Justin Martin on 5/15/13.
+//  Copyright (c) 2013 Two Wing Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WMAppDelegate class]));
+    }
+}
